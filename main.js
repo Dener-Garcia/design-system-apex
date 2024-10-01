526a6f3416224b758b1298d344eeaf801acbda80
= <dener.criarbr@gmail.com>
2024-10-01 16:04:11 -0300

    start project

---
+++ main.js
@@ -0,0 +1,6 @@
+import './style.css'
+import "./src/utils/lucide"
+import heroIllustration from "./src/components/heroIllustration"
+import introductionIllustration from "./src/components/introductionIllustration"
+
+
---
+++ src/utils/lucide.js
@@ -0,0 +1,18 @@
+import {
+    createIcons,
+    Users,
+    ChartSpline,
+    MonitorCheck,
+    Puzzle 
+} from 'lucide';
+
+createIcons(
+    {
+        icons: {
+            Users,
+            ChartSpline,
+            MonitorCheck,
+            Puzzle 
+        }
+    }
+)
