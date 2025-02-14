2c97cce6763d13dd0717743e8421c1d67f4ddb78
Dener Garcia <dener.criarbr@gmail.com>
2025-02-14 12:37:10 -0300

    fix: btn danger pasto code power apps

--- src/components-dsa/buttons/BtnDangerDsa/BtnDangerDsa.js
+++ src/components-dsa/buttons/BtnDangerDsa/BtnDangerDsa.js
@@ -3,84 +3,61 @@ import "./BtnDangerDsa.css"
 
 const BtnDangerDsa = document.createElement("div")
 
-const codeSnippet = `- BtnIcnLfDsa:
-    Control: GroupContainer
-    Variant: manualLayoutContainer
+const codeSnippet = `- BtnDangerDsa:
+    Control: Classic/Button
     Properties:
-      DropShadow: =DropShadow.None
-      Height: =40
+      BorderColor: =ColorValue(dsaTokens.colorFeedbackError)
+      Color: |-
+        =ColorValue(
+        dsaTokens.colorContrastDark
+        )
+      DisabledBorderColor: |-
+        =ColorValue(
+        dsaTokens.colorNeutral300
+        )
+      DisabledColor: |-
+        =ColorValue(
+        dsaTokens.colorNeutral300
+        )
+      DisabledFill: |-
+        =ColorValue(
+        dsaTokens.colorAccentSecondary
+        )
+      Fill: |-
+        =ColorValue(
+        dsaTokens.colorFeedbackErrorAccent
+        )
+      FocusedBorderColor: |-
+        =ColorValue(
+        dsaTokens.colorNeutral200
+        )
+      FocusedBorderThickness: '=2 '
+      HoverBorderColor: |-
+        =ColorValue(
+        dsaTokens.colorFeedbackErrorAccent
+        )
+      HoverColor: |-
+        =ColorValue(
+        dsaTokens.colorFeedbackErrorAccent
+        )
+      HoverFill: =ColorValue(dsaTokens.colorFeedbackError)
+      PaddingBottom: =8
+      PaddingLeft: =16
+      PaddingRight: =16
+      PaddingTop: =8
+      PressedBorderColor: |-
+        =ColorValue(
+        dsaTokens.colorFeedbackError
+        )
+      PressedColor: =ColorValue(dsaTokens.colorFeedbackErrorAccent)
+      PressedFill: =ColorValue(dsaTokens.colorFeedbackError)
       RadiusBottomLeft: =200
       RadiusBottomRight: =200
       RadiusTopLeft: =200
       RadiusTopRight: =200
-      Width: =160
-      X: =727
-      Y: =150
-    Children:
-    - btniconLt:
-        Control: Classic/Button
-        Properties:
-          BorderColor: |-
-            =ColorValue(
-            dsaTokens.colorAccentPrimary
-            )
-          Color: |-
-            =ColorValue(
-            dsaTokens.colorContrastDark
-            )
-          Fill: |-
-            =ColorValue(
-            dsaTokens.colorAccentPrimary
-            )
-          Height: =Parent.Height
-          HoverBorderColor: |-
-            =ColorValue(
-            dsaTokens.colorAccentPrimary
-            )
-          HoverColor: "=ColorValue(
-dsaTokens.colorContrastDark
-)   "
-          HoverFill: |-
-            =ColorValue(
-            dsaTokens.colorAccentPrimaryHover
-            )
-          PaddingBottom: =8
-          PaddingLeft: '=8   '
-          PaddingRight: =8
-          PaddingTop: =8
-          PressedBorderColor: |-
-            =ColorValue(
-            dsaTokens.colorAccentSecondaryHover
-            )
-          PressedColor: "=ColorValue(
-dsaTokens.colorContrastDark
-)   "
-          PressedFill: |-
-            =ColorValue(
-            dsaTokens.colorAccentPrimaryHover
-            )
-          RadiusBottomLeft: =200
-          RadiusBottomRight: =200
-          RadiusTopLeft: =200
-          RadiusTopRight: =200
-          Size: =16
-          Width: =Parent.Width
-          Y: '=0   '
-    - btnIcnLt:
-        Control: Classic/Icon
-        Variant: Add
-        Properties:
-          Color: |-
-            =If(
-                btniconLt.DisplayMode = DisplayMode.Disabled,
-                ColorValue(dsaTokens.colorNeutral300),
-                ColorValue(dsaTokens.colorContrastDark)
-            )
-          Height: =btniconLt.Size
-          Icon: =Icon.ChevronDown
-          Width: =btniconLt.Size
-          X: = Self.Width + 8
-          Y: =btniconLt.Height /2 - Self.Height /2
+      Size: =dsaTokens.fontText
+      X: =1089
+      Y: =237
 `
 
 BtnDangerDsa.setAttribute('data-code', codeSnippet)
