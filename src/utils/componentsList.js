a900d791f66c2f55361c387646a28812184bb5a6
Dener Garcia <dener.criarbr@gmail.com>
2025-05-07 00:44:58 +0000

    style: add animatino components-bar and still open details minus and add icon

--- src/utils/componentsList.js
+++ src/utils/componentsList.js
@@ -24,7 +24,7 @@ import PageTemplateDsa from "../components-dsa/page-template/PageTemplateDsa";
 import CardHzDsa from "../components-dsa/cards/CardHzDsa/CardHzDsa";
 import CardThumbDsa from "../components-dsa/cards/CardThumbDsa/CardThumbDsa";
 import CardSmDsa from "../components-dsa/cards/CardSmDsa/CardSmDsa";
-import cardSmBtnDsa from "../components-dsa/cards/CardSmBtnDsa/cardSmBtnDsa";
+import cardSmBtnDsa from "../components-dsa/cards/CardSmBtnDsa/CardSmBtnDsa";
 
 import ModalDsa from "../components-dsa/modal/ModalDsa";
 
